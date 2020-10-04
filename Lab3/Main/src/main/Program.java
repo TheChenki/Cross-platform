1d41283b7c484a91ee47fa69b41e0e266793b16d
Denis Keshishyan <nikio.chenki@gmail.com>
2020-10-04 15:25:40 +0300

    Lab3 без последнего

--- Lab3/Main/src/main/Program.java
+++ Lab3/Main/src/main/Program.java
@@ -1,23 +1,44 @@
 package main;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
-
 import SignalLib.calculations.*;
+import SignalLib.io.FileReader;
 
 public class Program
 {
     public static void main(String[] args) throws IOException
     {
-        List<Integer> numbers = readFile("D:\\14.bin");
+        List<Integer> numbers = null;
+
+        if (args.length == 0)
+        {
+            System.out.println("Укажите путь к файлу");
+            return;
+        }
+
+        try
+        {
+            numbers = FileReader.readFile(args[0]);
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("Файл не найден");
+            return;
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Ошибка файла");
+            return;
+        }
+
         SignalCharacteristicsCalculator signalCalculator = new SignalCharacteristicsCalculator();
 
         Integer dynamicRange = signalCalculator.calculateDynamicRange(numbers);
         Integer signalEnergy = signalCalculator.calculateSignalEnergy(numbers);
         Float avgSignalStrength = signalCalculator.calculateAverageSignalStrength(numbers);
-        Float avgSignalSamplesValue =  signalCalculator.calculateAverageSignalSamplesValue(numbers);
+        Float avgSignalSamplesValue = signalCalculator.calculateAverageSignalSamplesValue(numbers);
         Float signalSampleValuesVariance = signalCalculator.calculateSignalSampleValuesVariance(numbers);
 
         System.out.println(dynamicRange);
@@ -25,27 +46,5 @@ public class Program
         System.out.println(avgSignalStrength);
         System.out.println(avgSignalSamplesValue);
         System.out.println(signalSampleValuesVariance);
-
-    }
-
-    public static List<Integer> readFile(String path) throws IOException
-    {
-        List<Integer> list = new ArrayList<Integer>();
-        FileInputStream inputStream = new FileInputStream(path);
-
-        int readResult;
-        do
-        {
-            readResult = inputStream.read();
-
-            if (readResult != -1)
-            {
-                list.add(readResult);
-            }
-
-        }
-        while (readResult != -1);
-
-        return list;
     }
 }
---
+++ Lab3/SignalLib/src/SignalLib/io/FileReader.java
@@ -0,0 +1,30 @@
+package SignalLib.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader
+{
+    public static List<Integer> readFile(String path) throws IOException
+    {
+        List<Integer> list = new ArrayList<Integer>();
+        FileInputStream inputStream = new FileInputStream(path);
+
+        int readResult;
+        do
+        {
+            readResult = inputStream.read();
+
+            if (readResult != -1)
+            {
+                list.add(readResult);
+            }
+
+        }
+        while (readResult != -1);
+
+        return list;
+    }
+}
